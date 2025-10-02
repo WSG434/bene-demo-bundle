dd7380697325102a728f1ffb7ea4c7181f5fb0f3
Владимир Гордеев <vladimir.gordeev@infourok.biz>
2025-10-02 16:56:28 +0300

    fix

--- src/DependencyInjection/BeneDemoExtension.php
+++ src/DependencyInjection/BeneDemoExtension.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Bene\DemoBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
@@ -17,13 +16,13 @@ final class BeneDemoExtension extends Extension
     }
     public function load(array $configs, ContainerBuilder $container): void
     {
-//    $configuration = new Configuration();
-//    $config = $this->processConfiguration($configuration, $configs);
+    $configuration = new Configuration();
+    $config = $this->processConfiguration($configuration, $configs);
 
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
     $loader->load('services.yaml');
 
     // пример: прокинуть параметр в сервисы
-//    $container->setParameter('bene_demo.greeting', $config['greeting']);
+    $container->setParameter('bene_demo.greeting', $config['greeting']);
     }
 }
