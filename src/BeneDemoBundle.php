52f6110fc8a60bb21fb3b638bb7eecae3d0395d4
Владимир Гордеев <vladimir.gordeev@infourok.biz>
2025-10-02 16:14:04 +0300

    fix

--- src/BeneDemoBundle.php
+++ src/BeneDemoBundle.php
@@ -3,8 +3,14 @@ declare(strict_types=1);
 
 namespace Bene\DemoBundle;
 
+use Bene\DemoBundle\DependencyInjection\BeneDemoExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class BeneDemoBundle extends Bundle {
 
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new BeneDemoExtension(); // важно
+    }
 }
